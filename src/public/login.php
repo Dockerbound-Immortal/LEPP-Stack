1fe67b415ef1c28af02a1d7588084fa2da513a71
Luke McCann <developer-lukemccann@outlook.com>
2020-06-14 13:52:20 +0100

    Add echo for username and password test

--- src/public/login.php
+++ src/public/login.php
@@ -3,6 +3,9 @@
     if(isset($_POST['submit'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
+
+        echo "Username: " . $username . "<br />";
+        echo "Password: " . $password;
     }
 
 ?>
